34a26d7a812681c76fe28dd2593d395a2290c9b0
Phillip Pearson <pp@myelin.co.nz>
2008-07-14 18:26:36 +1200

    implemented try_acquire

--- test.rb
+++ test.rb
@@ -8,14 +8,17 @@ path = "/testing_node"
 
 puts "working with path #{path}"
 
-begin
-  puts "exists: #{z.exists(path).inspect}"
-rescue Zookeeper::NoNodeError
-  puts "doesn't exist"
-  stat = nil
+stat = z.stat(path)
+puts "exists? #{stat.inspect}"
+
+unless stat.nil?
+  z.ls(path).each do |o|
+    puts "  child object: #{o}"
+  end
+  puts "delete: #{z.delete(path, stat.version).inspect}"
 end
 
-puts "create: #{z.create(path, "initial value", 0).inspect}" unless stat
+puts "create: #{z.create(path, "initial value", 0).inspect}"
 
 value, stat = z.get(path)
 puts "current value #{value}, stat #{stat.inspect}"
@@ -24,6 +27,16 @@ puts "set: #{z.set(path, "this is a test", stat.version).inspect}"
 
 value, stat = z.get(path)
 puts "new value: #{value.inspect} #{stat.inspect}"
+
+puts "let's try using a lock"
+z.try_acquire("/test_lock", "this is the content of the lock file") do |have_lock|
+  puts have_lock ? "we have the lock!" : "failed to obtain lock :("
+  if have_lock
+    puts "sleeping 10 secs"
+    sleep 10
+  end
+end
+puts "done with locking..."
 
 puts "delete: #{z.delete(path, stat.version).inspect}"
 
--- zookeeper.rb
+++ zookeeper.rb
@@ -15,8 +15,40 @@ class Zookeeper < CZookeeper
     ZkStat.new(super)
   end
 
+  def stat(path)
+    exists(path)
+  rescue Zookeeper::NoNodeError
+    nil
+  end
+
   def get(path)
     value, stat = super
     [value, ZkStat.new(stat)]
+  end
+
+  def try_acquire(path, value)
+    create(path, "lock node", 0) unless stat(path)
+
+    # attempt to obtain the lock
+    realpath = create("#{path}/lock-", value, Zookeeper::EPHEMERAL | Zookeeper::SEQUENCE)
+    puts "created lock node #{realpath}"
+
+    # see if we got it
+    serial = /lock-(\d+)$/.match(realpath).captures[0].to_i
+    child_serials = ls(path).map { |child|
+      if m = /lock-(\d+)$/.match(child)
+        m.captures[0].to_i
+      else
+        nil
+      end
+    } .reject { |n| n.nil? }
+    puts "  my serial is #{serial}; child serials are #{child_serials.inspect}"
+    have_lock = (serial == child_serials.min)
+
+    # call block
+    yield(have_lock)
+
+    # release the lock
+    delete(realpath, stat(realpath).version)
   end
 end
