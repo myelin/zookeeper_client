b963d8bf5bb46ab24d4ba0fb1dd96a4f92a85516
Evan Weaver <eweaver@twitter.com>
2010-01-23 20:39:06 -0800

    Link.

--- ext/zookeeper_c/extconf.rb
+++ ext/zookeeper_c/extconf.rb
@@ -1,9 +1,13 @@
 require 'mkmf'
 
+dir_config("zookeeper")
+
 have_header "sys/errno.h" 
 have_header "stdio.h"
-find_header("zookeeper/zookeeper.h", dir_config("zookeeper").first || ENV['INCLUDE']) or
-  raise "zookeeper.h not found. Try 'gem install zookeeper -- --with-zookeeper-dir=/SOMEWHERE'"
+find_header("zookeeper/zookeeper.h") or
+  raise "zookeeper.h not found."
+find_library("zookeeper_st", "zoo_set_debug_level") or 
+  raise "libzookeeper_st not found."
   
 dir_config('zookeeper_c')
 create_makefile( 'zookeeper_c')
