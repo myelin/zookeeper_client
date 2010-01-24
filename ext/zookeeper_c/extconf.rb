066910ab3e1d09bb46dcc13de37899fdb4af4f9e
Eric Maland <eric@utility002.twitter.com>
2010-01-24 06:44:49 +0000

    move back to mt zk lib

--- ext/zookeeper_c/extconf.rb
+++ ext/zookeeper_c/extconf.rb
@@ -6,8 +6,8 @@ have_header "sys/errno.h"
 have_header "stdio.h"
 find_header("zookeeper/zookeeper.h") or
   raise "zookeeper.h not found."
-find_library("zookeeper_st", "zoo_set_debug_level") or 
-  raise "libzookeeper_st not found."
+find_library("zookeeper_mt", "zoo_set_debug_level") or 
+  raise "libzookeeper_mt not found."
   
 dir_config('zookeeper_c')
 create_makefile( 'zookeeper_c')
